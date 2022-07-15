c679626fed980ea44efb25720d31b7563215feb6
opozdaykO <no_oname@bk.ru>
2022-07-15 15:22:02 +0500

    Initial commit

---
+++ tgbot.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/Syfaro/telegram-bot-api"
+	//"github.com/lib/pq"
+	"log"
+)
+
+const TGBotTokken = "5537584496:AAER6fpsUAOvMfQGWBmo6WLjs1HS5y9MFMg"
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(TGBotTokken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+}
